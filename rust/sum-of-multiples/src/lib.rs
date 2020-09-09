d3fb22b75d513a31c9b76f7e69ed4f980819b7a7
DaviRain-Su <davirain.yin@gmail.com>
2020-09-09 23:04:59 +0800

    add sum of multiples

---
+++ rust/sum-of-multiples/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    (1..limit).filter(|value|{
+        factors.iter().any(|item|{
+            *item != 0 && value % item == 0
+        })
+    }).sum()
+    
+}
